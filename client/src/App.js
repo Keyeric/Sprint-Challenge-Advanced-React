169ac4e490774c21b730cb03b902b55e17ef3368
Key Bristol <keyeric.bristol@gmail.com>
2020-02-14 17:53:43 +0000

    MVP with minor testing >_>

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Nav from "./components/Nav"
+
+class App extends React.Component {
+
+    state = {
+
+        poke: [],
+        error: ''
+
+    };
+
+    componentDidMount() {
+        axios
+            .get(`https://pokeapi.co/api/v2/pokemon/`)
+            .then(res => {
+                //     console.log(res),
+                //     console.log(res.data),
+                //     console.log(res.data.results),
+                    this.setState({
+                        poke: res.data.results
+                    });
+            })
+            .catch(err => console.log("I didn't choose you", err));
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                
+                    <Nav />
+
+                    {this.state.poke.map(attack => (
+                        <div className="pokeball" key={attack.name}>
+                            <h2>{attack.name}</h2>
+                            <Link to={attack.url} />
+                        </div>
+                    ))}
+
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
